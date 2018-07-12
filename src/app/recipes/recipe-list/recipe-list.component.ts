8af757e21b773ad332094f4d7ec954d59d767c64
te <tarek.elmeskine@eurekanetwork.org>
2018-07-12 18:52:26 +0200

    - passing recipe data with property binding and event

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Recipe} from '../recipe.model';
 
 @Component({
@@ -11,10 +11,13 @@ export class RecipeListComponent implements OnInit {
     new Recipe('A Test Recipe',
       'This is simply a test',
       'https://img-3.journaldesfemmes.fr/h5BMkASKhHha-Rk1OhqTVN_ModM=/750x/smart/7ec9c7cc57a844c4acf11ea8313519b2/recipe-jdf/10018962.jpg'),
-    new Recipe('A Test Recipe',
-      'This is simply a test',
-      'https://img-3.journaldesfemmes.fr/h5BMkASKhHha-Rk1OhqTVN_ModM=/750x/smart/7ec9c7cc57a844c4acf11ea8313519b2/recipe-jdf/10018962.jpg')
+    new Recipe('Another Test Recipe',
+      'Another test recipe that seems very good',
+      'https://imagesvc.timeincapp.com/v3/mm/image?url=http%3A%2F%2Fcdn-image.myrecipes.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2Fimage%2Frecipes%2Fck%2F11%2F04%2Ffettuccine-olive-oil-ck-x.jpg%3Fitok%3DN9u99OOY&w=800&q=85')
   ];
+
+  @Output()
+  recipeWasSelected = new EventEmitter<Recipe>();
 
   constructor() {
   }
@@ -22,4 +25,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit() {
   }
 
+  onRecipeSelected(recipe : Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
