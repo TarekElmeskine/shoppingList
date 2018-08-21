dcfdbb9b8b9c47deb856e7a337d49c3c6827048e
te <tarek.elmeskine@eurekanetwork.org>
2018-08-21 18:11:01 +0200

    - fix bugs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,13 +11,12 @@ import {ShoppingListComponent} from './shopping-list/shopping-list.component';
 import {ShoppingEditComponent} from './shopping-list/shopping-edit/shopping-edit.component';
 import {FormsModule} from '@angular/forms';
 import {DropdownDirective} from './shared/dropdown.directive';
-import {ShoppingListService} from "./shopping-list/shopping-list.service";
-import {AppRoutingModule} from "./app-routing.module";
-import {RecipesResolverService} from "./recipes/recipes-resolver.service";
-import {RecipeService} from "./recipes/recipe.service";
+import {ShoppingListService} from './shopping-list/shopping-list.service';
+import {AppRoutingModule} from './app-routing.module';
+import {RecipesResolverService} from './recipes/recipes-resolver.service';
+import {RecipeService} from './recipes/recipe.service';
 import {RecipeStartComponent} from './recipes/recipe-start/recipe-start.component';
 import {RecipeEditComponent} from './recipes/recipe-edit/recipe-edit.component';
-import {IngredientComponent} from './shopping-list/ingredient/ingredient.component';
 
 @NgModule({
   declarations: [
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,6 +1,5 @@
 import {Ingredient} from '../shared/ingredient.model';
 import {Subject} from 'rxjs/Subject';
-import {Subject} from "rxjs/index";
 
 export class ShoppingListService {
 
